7746d8e226d0a8c8a4bc4a386e7ffaa5733d1149
BelkaLu <levikina.ea@gmail.com>
2023-08-22 04:50:49 +0300

    t

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Бонусные милли");
+        int x = 20000;
+        int y;
+        y = (int) x / 20;
+        System.out.println(y);
+    }
+}
