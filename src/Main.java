f3845164e58c857618c76db00abd8343723f2685
BelkaLu <levikina.ea@gmail.com>
2023-08-25 00:36:27 +0300

    fix name

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Бонусные милли");
         int payment = 20000;
-        int y;
-        y = (int) payment / 20;
-        System.out.println(y);
+        int mile;
+        mile = (int) payment / 20;
+        System.out.println(mile);
     }
 }
